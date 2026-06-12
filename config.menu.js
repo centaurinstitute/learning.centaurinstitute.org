d6356b7b6a9a697308b45e6c2a5bfadbb58d3c04
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2026-06-12 17:48:21 +0300

    Comment out

--- config.menu.js
+++ config.menu.js
@@ -1,5 +1,5 @@
 import ActionButtons from "./src/components/ActionButton/ActionButtons";
-import VideoSearch from "./src/components/Search/VideoSearch";
+// import VideoSearch from "./src/components/Search/VideoSearch";
 const menuConfig = {
   topMenu: [],
   sideMenu: [
