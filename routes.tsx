26bebe09d0ffefc3ff52f52ef90fd058b8f0eac8
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2026-06-12 16:01:57 +0300

    Update routes

--- routes.tsx
+++ routes.tsx
@@ -6,10 +6,12 @@ import React from "react";
 import SS2022 from "./src/pages/ss2022";
 import SS2023 from "./src/pages/ss2023";
 import SS2024 from "./src/pages/ss2024";
+import SS2025 from "./src/pages/ss2025";
 import VideoDetail from "./src/components/VideoDetail/VideoDetail";
 import WW2022 from "./src/pages/ww2022";
 import WW2023 from "./src/pages/ww2023";
 import WW2024 from "./src/pages/ww2024";
+
 const routes = [
   {
     container: <Container />,
@@ -20,6 +22,10 @@ const routes = [
           {
             path: "/learning",
             element: <Index />,
+          },
+          {
+            path: "/learning/2025",
+            element: <SS2025 />,
           },
           {
             path: "/learning/2024",
