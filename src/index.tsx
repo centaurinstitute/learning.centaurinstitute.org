fd5aa056f65c4b5e0a0740c55069f05129d5aa02
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2026-08-04 15:09:06 +0300

    Add global css to change style and fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import "./global.css";
+
 import { Platform } from "@canmingir/link";
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -6,5 +8,5 @@ import routes from "../routes";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Platform routes={routes} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
